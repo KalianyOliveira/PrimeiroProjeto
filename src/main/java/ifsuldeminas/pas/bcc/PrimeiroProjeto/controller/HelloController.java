dc7357fd10e24abf2d2dba25ac66cbdadc274d9b
Kaliany Oliveira Lara <202111000019@IFSMP.PAS.IFSULDEMINAS.EDU.BR>
2023-09-12 11:33:56 -0300

    primeiro commit

---
+++ src/main/java/ifsuldeminas/pas/bcc/PrimeiroProjeto/controller/HelloController.java
@@ -0,0 +1,9 @@
+package ifsuldeminas.pas.bcc.PrimeiroProjeto.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public interface HelloController {
+}
